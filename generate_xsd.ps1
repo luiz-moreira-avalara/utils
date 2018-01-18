c49d143e884464875ba106ae7338fb690c541ba0
Luiz Guilherme Bauer Fraga Moreira <luiz.moreira@avalara.com>
2018-01-18 17:26:22 -0200

    Update generate_xsd.ps1

--- generate_xsd.ps1
+++ generate_xsd.ps1
@@ -2,4 +2,4 @@ $FileNames = @()
 Get-ChildItem -Recurse *.xsd | foreach {
   $FileNames += $_.FullName
 }
-xsd.exe /c /l:cs /n:Grc.Client.REINF.WebService.Models $FileNames
+xsd.exe /c /l:cs /n:MyProject.Xsds $FileNames
